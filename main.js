4616872ab8feec8e5c0faaa7d57e37ec5dfa25e3
BryanDevelops <orellanabryan911@gmail.com>
2021-12-28 23:01:36 -0500

    homepage additions
    homepage

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ const navSlide = () =>{
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
 
-
     burger.addEventListener('click',() =>{
         //toggle
         nav.classList.toggle('nav-active');
@@ -12,9 +11,12 @@ const navSlide = () =>{
        navLinks.forEach((link,index) => {
         if(link.style.animation){
             link.style.animation='';
+            document.getElementById("firstSection").style.marginTop = "48px";
+
         }
         else{
             link.style.animation= `navLinkFade 0.5s ease forwards ${index / 3 + .5}s`;
+            document.getElementById("firstSection").style.marginTop = "55vh";
 
              }
         });
