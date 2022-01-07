482eed0a0d9273ad7eef2d89cec0e09f1a1e4437
BryanDevelops <orellanabryan911@gmail.com>
2022-01-06 23:48:18 -0500

    Javascript effects
    Fade in Left
    Fade in Right
    Fade in Top

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
+const animated = document.querySelectorAll(".animated")
+const observer = new IntersectionObserver(entries =>{
+    console.log(entries)
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            document.querySelectorAll(".animated")[0].classList.add("fadeInLeft");
+            document.querySelectorAll(".animated")[1].classList.add("fadeInTop");
+            document.querySelectorAll(".animated")[2].classList.add("fadeInRight");
+    
+        }
+    })
+    })
+    
+    observer.observe(document.querySelector(".container-fluid"));
+    observer.observe(animated[0])
+
+function flipCard(){
+    this.classList.toggle('is-flipped');
+
+}
+
+document.getElementById("cardBryan").addEventListener("click", flipCard);
+document.getElementById("cardTwo").addEventListener("click", flipCard);
+document.getElementById("cardThree").addEventListener("click", flipCard);
+
+
 const navSlide = () =>{
     const burger= document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks = document.querySelectorAll('.nav-links li');
+
 
     burger.addEventListener('click',() =>{
         //toggle
@@ -11,17 +38,14 @@ const navSlide = () =>{
        navLinks.forEach((link,index) => {
         if(link.style.animation){
             link.style.animation='';
-            document.getElementById("firstSection").style.marginTop = "48px";
-
         }
         else{
             link.style.animation= `navLinkFade 0.5s ease forwards ${index / 3 + .5}s`;
-            document.getElementById("firstSection").style.marginTop = "55vh";
 
              }
         });
         //Burger
-        burger.classList.toggle('toggle');
+        burger.classList.toggle('tog gle');
     
     });
  
@@ -29,3 +53,5 @@ const navSlide = () =>{
 }
 
 navSlide(); 
+
+
